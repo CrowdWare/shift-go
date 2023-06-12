a4873dbf221272972bb1bf314ff6d87208131c85
Artanidos <japp.olaf@gmail.com>
2023-06-12 23:31:51 +0200

    upd

--- lib/lib.go
+++ lib/lib.go
@@ -3,12 +3,14 @@ package lib
 var dbFile string
 var secretKey []byte
 var account Account
+var algorithm string
 
 const (
-	algorithm = "AES/GCM/NoPadding"
+	algoEnc = "44880922281e9a7d9ba243b2599fcbcf41fd8199b0d059a7b373abdc5f0447380b13b620239871ba3b7db08ca8"
 )
 
 func Init(filesDir string) {
 	dbFile = filesDir + "/shift.db"
 	generateSecretKey()
+	algorithm = DecryptStringGCM(algoEnc)
 }
