a59187920890d75d73a716eb22390858cfe70a62
Artanidos <japp.olaf@gmail.com>
2023-06-18 18:27:18 +0200

    upd

--- release.sh
+++ release.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+version="1.0.0"
 go run . vars
 mv ./lib/crypto_vars.go.temp ./lib/crypto_vars.go
 
@@ -9,5 +10,5 @@ export GOOS=android
 export CGO_ENABLED=1
 export ANDROID_NDK_HOME=/Volumes/LaCie/Android/sdk/ndk/25.2.9519653
 # Build the AAR file for all architectures
-gomobile bind -androidapi 28 -target=android -o libshift-release.aar github.com/crowdware/shift-go/lib
-cp libshift-release.aar /Users/art/SourceCode/Shift/Android/app/libs
+gomobile bind -androidapi 28 -target=android -o libshift-release-$version.aar github.com/crowdware/shift-go/lib
+cp libshift-release-$version.aar /Users/art/SourceCode/Shift/Android/app/libs
