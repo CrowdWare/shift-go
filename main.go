bbf1e15a404c7103f4c5e68541bebcedbbb7e155
Artanidos <japp.olaf@gmail.com>
2023-06-18 18:13:37 +0200

    upd

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
+	"os"
 	"regexp"
+	"strconv"
+	"time"
 
 	"github.com/crowdware/shift-go/lib"
 )
@@ -38,25 +43,123 @@ func readPythonConfig(filePath string) (string, string, string, error) {
 	return apiKey, secretkey, storj, nil
 }
 
-func main() {
-	fmt.Println("Shift")
+func generateRandomNumber(min, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(max-min+1) + min
+}
 
-	configFilePath := "config.py"
+func writeVars() {
+	file, err := os.Create("./lib/crypto_vars.go.temp")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
 
-	api_key, secret_key, storj_access_token, err := readPythonConfig(configFilePath)
+	writer := bufio.NewWriter(file)
+
+	text := "package lib\n\n"
+	text += "var variable1 = " + strconv.Itoa(generateRandomNumber(1000, 9999)) + "\n"
+	text += "var variable2 = " + strconv.Itoa(generateRandomNumber(1000, 9999)) + "\n"
+	text += "var variable3 = " + strconv.Itoa(generateRandomNumber(1000, 9999)) + "\n"
+	text += "var variable4 = " + strconv.Itoa(generateRandomNumber(100000, 999999)) + "\n"
+	text += "var variable5 = " + strconv.Itoa(generateRandomNumber(100000, 999999)) + "\n"
+	text += "\n"
+	text += "const secret_key_enc = \"\"\n"
+	text += "const api_key_enc = \"\"\n"
+	text += "const service_url_enc = \"\"\n"
+	text += "const storjAccessTokenEnc = \"\"\n"
+
+	_, err = writer.WriteString(text)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+	err = writer.Flush()
+	if err != nil {
+		fmt.Println("Error flushing writer:", err)
+		return
+	}
+}
+
+func secret() {
+	infile, err := os.Open("./lib/crypto_vars.go")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer infile.Close()
+
+	scanner := bufio.NewScanner(infile)
+	text := ""
+	numLines := 7
+	lineCount := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		text += line + "\n"
+		lineCount++
+		if lineCount == numLines {
+			break
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+	file, err := os.Create("./lib/crypto_vars.go.temp")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+
+	text += "\n"
+
+	api_key, secret_key, storj_access_token, err := readPythonConfig("shift_keys.py")
 	if err != nil {
 		fmt.Println("Error reading Python config:", err)
 		return
 	}
 	secret_key_enc := lib.Encrypt(secret_key)
-	fmt.Println("const secret_key_enc = \"" + secret_key_enc + "\"")
+	text += "const secret_key_enc = \"" + secret_key_enc + "\"\n"
 
 	api_key_enc := lib.Encrypt(api_key)
-	fmt.Println("const api_key_enc = \"" + api_key_enc + "\"")
+	text += "const api_key_enc = \"" + api_key_enc + "\"\n"
 
 	service_url_enc := lib.Encrypt("http://shift.crowdware.at:8080/")
-	fmt.Println("const service_url_enc = \"" + service_url_enc + "\"")
+	text += "const service_url_enc = \"" + service_url_enc + "\"\n"
 
 	storjAccessTokenEnc := lib.Encrypt(storj_access_token)
-	fmt.Println("const storjAccessTokenEnc = \"" + storjAccessTokenEnc + "\"")
+	text += "const storjAccessTokenEnc = \"" + storjAccessTokenEnc + "\"\n"
+
+	_, err = writer.WriteString(text)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+	err = writer.Flush()
+	if err != nil {
+		fmt.Println("Error flushing writer:", err)
+		return
+	}
+}
+
+/*
+**	Initialize secret keys.
+**	First exchange variable1...variable5 in crypto
+ */
+func main() {
+	if len(os.Args) > 1 {
+		if os.Args[1] == "vars" {
+			writeVars()
+		} else if os.Args[1] == "secret" {
+			secret()
+		}
+	} else {
+		fmt.Println("Shift")
+		fmt.Println("Usage: go run . <arg> | where args are: vars | secret")
+	}
 }
--- todo.md
+++ todo.md
@@ -1,26 +1,4 @@
 # Todo
 
-- Database for message storage (StorJ, libp2p dht)
-    On Storj you have 25 GB for free to store your own data. The user could create a bucket for shift and donate it to the shift movement.
-    Storj is decentral and more secure than libp2p.
-    
-- No service has to run in background, because we are not going P2P.
-- Scooping is done all 20 hours, user just has to start it on a daily base.
-- Giving is now as follows:
-    Receiver shows QR code or sends a hex code with a proposal (public key, amount, purpose, isOnline=can ping storj)
-    Giver accepts the proposal and stores a record for the receiver on storj if isOnline is set.
-    Receiver pulls the record from storj.
-
-    If giver or receiver is offline we will fallback to QR code transmission.  
-    Alternatively instead of storing a record on storj the giver can create a QR code and receiver scan that record.
-    In both cases sending a record or creating a QR code withdraws the amount from giver account and scanning or downloading the record books the amount to receivers account. The record is only useful for the receiver and will loose the validity after x days.
-
-    In the case that a record cannot be pulled by the receiver. The receiver has to inform the giver. Than the giver pulls the record back from storj and the amount is booked to the account.
-
-    A record has the following key:   public key of the receiver - "LMP" - public key of giver
-
-    QR code first !!!
-
-- REST calls are optional, so when REST call fails, the client will continue to operate, no warnings will be shown
-    Invite code, name, etc are optional the user does not have to enter these values
-    
+- Releases should be build on the server using the same keys as the webservice
+- The version of the lib should be packed in the shift.db first, last bytes or timestamp of the db file, then we can deploy multiple versions of the lib in the same apk and on the client the logic can choose which lib to use, to be backward compatible. First read version from dbfile then choose
