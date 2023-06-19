0c719e403fbef39ee3b326b9bb7c9f3ef4316e1c
Artanidos <712020:22347816-c829-4a08-8d24-a1785aa4bc8d@bots.bitbucket.org>
2023-06-19 23:25:05 +0200

    upd

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ export ANDROID_NDK_HOME=/Volumes/LaCie/Android/sdk/ndk/25.2.9519653
 
 # Build the AAR file for all architectures
 gomobile bind -androidapi 28 -target=android -tags debug -o libshift-debug-$version.aar github.com/crowdware/shift-go/lib
-cp libshift-debug-$version.aar /Users/art/SourceCode/Shift/Android/app/libs
+cp libshift-debug-$version.aar /Users/art/SourceCode/shift/Android/app/libs
