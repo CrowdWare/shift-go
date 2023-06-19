9c569aef3cfd2f2efdaad19975eb85d01911ae0b
Artanidos <712020:22347816-c829-4a08-8d24-a1785aa4bc8d@bots.bitbucket.org>
2023-06-20 01:27:43 +0200

    upd

--- shift_keys.py
+++ shift_keys.py
@@ -1,4 +1,4 @@
 SHIFT_API_KEY = "<api key>"
 SHIFT_CLIENT_KEY = "<client key>"
 SHIFT_SECRET_KEY = "<secret key>"
-STORJ_ACCESS_TOKEN = "<storj access token>"
+STORJ_ACCESS_TOKEN = "<storj access key>"
